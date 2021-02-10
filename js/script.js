ef103dfeafec1e21fd38db51edecf2f30e24a493
jpNew-man <78013903+jpNew-man@users.noreply.github.com>
2021-02-10 17:34:29 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -56,35 +56,23 @@ $('.sec4 #contact #call p:nth-child(1)').click(function(){
     });
 });
 
-
-$('.sec3 .work7 #computer').mouseover(function(){
+$('.sec3 .work8 #computer, #space, #duck').mouseover(function(){
     $(this).children('.up').stop().animate({bottom:0});
 });
-$('.sec3 .work7 #computer').mouseout(function(){
-    $(this).children('.up').stop().animate({bottom:-120});
-});
-$('.sec3 .work7 #space').mouseover(function(){
-    $(this).children('.up').stop().animate({bottom:0});
-});
-$('.sec3 .work7 #space').mouseout(function(){
-    $(this).children('.up').stop().animate({bottom:-120});
-});
-$('.sec3 .work7 #duck').mouseover(function(){
-    $(this).children('.up').stop().animate({bottom:0});
-});
-$('.sec3 .work7 #duck').mouseout(function(){
+$('.sec3 .work8 #computer, #space, #duck').mouseout(function(){
     $(this).children('.up').stop().animate({bottom:-120});
 });
 
-$('.work7 #computer').click(function(){
+
+$('.work8 #computer').click(function(){
     $('#modal').css("display","block");
     $('#modal .com').css("display","block");
 });
-$('.work7 #space').click(function(){
+$('.work8 #space').click(function(){
     $('#modal').css("display","block");
     $('#modal .spa').css("display","block");
 });
-$('.work7 #duck').click(function(){
+$('.work8 #duck').click(function(){
     $('#modal').css("display","block");
     $('#modal .duc').css("display","block");
 });
